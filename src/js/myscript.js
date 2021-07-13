e65442f2d5cab82cda5b184c14872b059561d8ab
earlchassee <one2earl@gmail.com>
2021-07-12 20:52:03 -0700

    Initial Condition

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,4 +1,15 @@
 console.log('Welcome to HackerU-JustCode Assignment');
+
+
+/*
+PURPOSE;
+    - Get data for phoneManufacturer records from server in JSON format
+    - GET URL
+        https://www.justcode.com/dataservice/api/phoneManufacturer/list
+    - Response data format: JSON
+*/
+
+
 $(document).ready(function() {
     getRemoteData();
 });
