01e09f2821c7a0c08880797ea251d91e635b2d57
earlchassee <one2earl@gmail.com>
2021-07-14 16:43:29 -0700

    Moving repo

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -9,6 +9,7 @@ PURPOSE;
     - Response data format: JSON
 */
 
+
 // Makes function available after document is loaded
 $(document).ready(function() {
     getRemoteData();
@@ -17,25 +18,30 @@ $(document).ready(function() {
 
 function getRemoteData() {
 // write your code here to fetch data from api.
-    try {
-        fetch(url)
-            .then(response => response.json())
-            // .then(data => console.log(data));     This works
-            .then(data => {
-                var temp = "";
-                data.forEach((itemData) => {
-                    temp += "<tr>"
-                    temp += "<td>" + itemData.id + "</td>"
-                    temp += "<td>" + itemData.manufacturerName + "</td>"
-                    temp += "<td>" + itemData.nameOfCEO + "</td>"
-                    temp += "<td>" + itemData.headquarters + "</td>"
-                    temp += "<td>" + itemData.modifiedOn + "</td>"
-                })
-                document.getElementById('data').innerHTML = temp
-            })
-    } catch (error) {
-        console.log(error)
-    }
+    fetch(url)
+        .then(response => response.json())
+        // .then(data => console.log(data));     This works
+        .then(data => {
+            console.log(data)
+            // What object data to write out
+            for (var x = 0; data.length; x++) {
+                var phoneMfr = data[x]
+                console.log(phoneMfr)
+                output1.innerHTML += phoneMfr.id + "<br>"
+                output2.innerHTML += phoneMfr.headquarters + "<br>"
+                output3.innerHTML += phoneMfr.manufacturerName + "<br>"
+                output4.innerHTML += phoneMfr.nameOfCEO + "<br>"
+                output5.innerHTML += phoneMfr.modifiedOn + "<br>"
+            }
+            // Where to write object data
+            var output1=document.getElementById('output1')
+            var output2=document.getElementById('output2')
+            var output3=document.getElementById('output3')
+            var output4=document.getElementById('output4')
+            var output5=document.getElementById('output5')
+        }
+        )
+        .catch(err => console.error(err))
 }
 
 getRemoteData()
